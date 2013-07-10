dc5fe36f3f36ff039371a8a5bcedb4b5f9c4c3b7
zambodi <zambodi@gmail.com>
2013-07-10 17:18:30 -0500

    Fix bug: 1513 - Jumps to top of list after selection (multiselect)
    While in multiselect/closeOnSelect=false mode, then list was jumping back to the top of the list after each selection/click.  Now the user will remain at the same location in the list after making a selection, which keeps them from having to scroll down each time and pickup where they left off in their selections.

--- select2.js
+++ select2.js
@@ -2690,7 +2690,7 @@ the specific language governing permissions and limitations under the Apache Lic
 
             this.opts.element.trigger({ type: "selected", val: this.id(data), choice: data });
 
-            if (this.select || !this.opts.closeOnSelect) this.postprocessResults();
+            if (this.select || !this.opts.closeOnSelect) this.postprocessResults(null, null, this.opts.closeOnSelect);
 
             if (this.opts.closeOnSelect) {
                 this.close();
